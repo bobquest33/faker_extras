6605c3d56637d2de60a667eda99b69bfeed4229b
Chris Tabor <christopher.tabor@emc.com>
2016-05-17 16:15:33 -0700

    Add binary options

--- faker_extras/binary.py
+++ faker_extras/binary.py
@@ -1,5 +1,7 @@
 """Faker data providers for binary data."""
 
+from random import choice
+
 from faker import Faker
 from faker.providers import BaseProvider
 
@@ -8,6 +10,38 @@ import utils
 
 class BinaryProvider(BaseProvider):
     """Provide binary data."""
+
+    byte_formats_binary = {
+        'KiB': 'kibibyte',
+        'MiB': 'mebibyte',
+        'GiB': 'gibibyte',
+        'TiB': 'tebibyte',
+        'PiB': 'pebibyte',
+        'EiB': 'exbibyte',
+        'ZiB': 'zebibyte',
+        'YiB': 'yobibyte',
+    }
+
+    byte_formats_decimal = {
+        'kB': 'kilobyte',
+        'MB': 'megabyte',
+        'GB': 'gigabyte',
+        'TB': 'terabyte',
+        'PB': 'petabyte',
+        'EB': 'exabyte',
+        'ZB': 'zettabyte',
+        'YB': 'yottabyte',
+    }
+
+    def binary_byte_str(self, code=False):
+        """Return a multiple of a byte, in its binary string form."""
+        bf = self.byte_formats_binary
+        return choice(bf.keys() if code else bf.values())
+
+    def decimal_byte_str(self, code=False):
+        """Return a multiple of a byte, in its typical decimal form."""
+        bf = self.byte_formats_decimal
+        return choice(bf.keys() if code else bf.values())
 
     def nibble(self):
         """Return a nibble.
@@ -45,6 +79,10 @@ class BinaryProvider(BaseProvider):
 if __name__ == '__main__':
     fake = Faker()
     fake.add_provider(BinaryProvider)
+    print(fake.binary_byte_str(code=True))
+    print(fake.decimal_byte_str(code=True))
+    print(fake.binary_byte_str())
+    print(fake.decimal_byte_str())
     print(fake.nibble())
     print(fake.octet())
     print(fake.byte())
